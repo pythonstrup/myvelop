fde212428305154f2246936fa051a91139a46542
Jonghyeok Park <jonghyeok.park@unitblack.co.kr>
2026-06-24 09:59:29 +0900

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'MyVelop';
+export const SITE_DESCRIPTION = 'My + Develop — 한 걸음씩 성장하는 개발 여정을 기록합니다.';
